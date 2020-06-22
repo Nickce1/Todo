95048a824cbf261e62e7748e45af0e55e9ee1b95
Nickce <f.lineup@gmail.com>
2020-06-22 17:38:06 +0400

    Insert query

--- server.js
+++ server.js
@@ -28,12 +28,11 @@ connection.connect(err => {
 
 const ALL_TASKS_QUERY = "SELECT * FROM TASK"
 
-app.get('/', (req, res) => {
-  // res.send('Hello world !')
-  const allTasks = connection.query ( ALL_TASKS_QUERY, ( error, result ) => {
+const QUERY_ALL = (res) => {
+  connection.query ( ALL_TASKS_QUERY, ( error, result ) => {
     if (error) {
       console.log("------------All tasks query error------------");
-      console.log("Query error : ");
+      console.log("Query all error : ");
       res.send(error)
     }
     else {
@@ -42,10 +41,32 @@ app.get('/', (req, res) => {
       })
     }
   })
+}
+
+app.get('/', (req, res) => {
+  QUERY_ALL(res)
 })
 
-app.get('/allTasks', (req, res) => {
-  
+// const name = "finish CRUD operations"
+// const state = false
+
+// const name = "Implement the back-end"
+// const state = true
+
+// connection.query ( `INSERT INTO task (t_name, t_etat) VALUES ( "${name}", ${state} );`)
+
+app.post('/insertTask', (req, res) => {
+  const insertQuery = `INSERT INTO task (t_name, t_etat) VALUES ("${req.t_name}", ${req.t_state})`
+  connection.query(insertQuery, (err, result) => {
+    if (err) {
+      console.log("------------InsertTask query error------------");
+      console.log("Insert query error : ");
+      res.send(error)
+    }
+    else {
+      QUERY_ALL(res)
+    }
+  })
 })
 
 app.listen(4000, () => {
