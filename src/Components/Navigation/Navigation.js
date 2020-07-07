6f65f52d4fd4608ca455a596a21caab27de219fc
Nickce <f.lineup@gmail.com>
2020-07-07 18:37:25 +0400

    Listed all the tasks matching to the search keywords

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,7 +3,6 @@ import Styled  from 'styled-components'
 import { Navbar, Form, FormControl, Button, NavDropdown } from 'react-bootstrap'
 
 import { AddTaskModal } from '../AddTaskModal'
-import { SearchContext } from '../Context'
 
 const Styles = Styled.div`
   @media screen and (max-width: 576px) {
@@ -43,36 +42,34 @@ const Navigation = () => {
   }
 
   return (
-    <SearchContext.Provider value={searchResult}>
-      <Styles>
-        <Navbar bg="dark" variant="dark" expand="sm">
-          <Navbar.Brand href="/">To Do App</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Button className="btn-100 ml-auto mr-2 my-sm-1" variant="success" onClick={handleShow}>
-              Add Task
-            </Button>
-            <NavDropdown.Divider className="d-md-none bg-transparent"/>
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                onChange={(event) => setInputSearch(event.target.value)}
-                value={inputSearch}
-                className="mr-sm-2 my-sm-1"
-              />
-              <a href="/search">
-                <Button variant="primary" className="btn-100 my-sm-1" onClick={submitSearch}>Search</Button>
-              </a>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-        <AddTaskModal
-          show = {show}
-          handleClose = {handleClose}
-        />
-      </Styles>
-    </SearchContext.Provider>
+    <Styles>
+      <Navbar bg="dark" variant="dark" expand="sm">
+        <Navbar.Brand href="/">To Do App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Button className="btn-100 ml-auto mr-2 my-sm-1" variant="success" onClick={handleShow}>
+            Add Task
+          </Button>
+          <NavDropdown.Divider className="d-md-none bg-transparent"/>
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              onChange={(event) => setInputSearch(event.target.value)}
+              value={inputSearch}
+              className="mr-sm-2 my-sm-1"
+            />
+            <a href="/search">
+              <Button variant="primary" className="btn-100 my-sm-1" onClick={submitSearch}>Search</Button>
+            </a>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <AddTaskModal
+        show = {show}
+        handleClose = {handleClose}
+      />
+    </Styles>
   )
 }
 
